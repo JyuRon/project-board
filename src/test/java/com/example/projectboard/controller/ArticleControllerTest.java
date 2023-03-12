e66206642385abc5dbd7b353aa4227de8ac91246
Jyuka <yoyksks3@gmail.com>
2023-03-12 14:12:20 +0900

    게시글 관련 뷰 테스트 작성 우선 어떤 경로로 뷰를 리열어줄지 테스트로 명세를 뽑는다. gradle build 시 테스트 실패가 발생하여 비활성화 처리(@Disabled)

---
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,85 @@
+package com.example.projectboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) // Controller Test 기법, 특정 클래스 지정 없을 경우 모든 controller 호ㅇ
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    // 실제 코드에서는 @Autowired를 생략이 가능하지만 테스트 코드의 경우 명시해줘야함
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현 중") // gradle build 시 테스트 실패가 발생하여 비활성화 처리
+    @DisplayName("[view][GET} 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET} 게시글 상세 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"))
+        ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET} 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+        ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET} 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search-hashtag"))
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+        ;
+    }
+
+}
