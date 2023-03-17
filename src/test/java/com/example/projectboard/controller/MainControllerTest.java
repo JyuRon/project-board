a72c4fc15b3e0a5b363e864e32ca12205601ce5d
Jyuka <yoyksks3@gmail.com>
2023-03-17 16:47:14 +0900

    루트 경로를 게시판 페이지로 forward 하도록 컨트롤러, 테스트 코드 수정 MainController 를 만들어서 기본 페이지가 게시판 페이지가 되도록 함 강의 중에서는 redirect 로 접근했는데 단순 조회 접근이고, url 루트 경로를 유지한 채로 게시판 페이지를 대표로 만들고 싶은 상황이므로 forward 가 더 적절해보임

--- src/test/java/com/example/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/example/projectboard/controller/MainControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(SecurityConfig.class) // // Spring Security 추가 이후 permit all 설정과 관련된 정보를 불러오지 못하여 에러 발생, 이를 해결
 @WebMvcTest(MainController.class)
@@ -26,7 +28,10 @@ class MainControllerTest {
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlePage() throws Exception {
 
         mvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection())
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print())
                 ;
     }
 }
