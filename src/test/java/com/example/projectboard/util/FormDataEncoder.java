b11fdd619c0752fac05a0b6551604f280147a247
Jyuka <yoyksks3@gmail.com>
2023-03-23 19:19:02 +0900

    <form> 데이터 전달 테스트용 인코더 추가
    - @TestComponent : 테스트 시 스프링으로 부터 bean 을 주입 받을 수 있도록 사용할 수 있다.
    - form data 는 api 와는 다른 형태로 직렬화하여 사용해야 한다.
    - 객체를 규약에 맞게 form data 포맷의 문자열로 바꿔주는 유틸리티를 테스트 전용으로 만들고, 테스트도 함께 작성
    - 이게 없으면 `post().param()` 형태로 값을 넣을 수도 있지만
    표현이 실제 post request 전송과 꼭 맞지 않는 듯 하고, 객체가 아니라 파라미터 단위로 넣어줘야 하므로 불편함.

---
+++ src/test/java/com/example/projectboard/util/FormDataEncoder.java
@@ -0,0 +1,46 @@
+package com.example.projectboard.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+@TestComponent
+public class FormDataEncoder {
+
+    private final ObjectMapper mapper;
+
+    public FormDataEncoder(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+
+    public String encode(Object obj) {
+        Map<String, String> fieldMap = mapper.convertValue(obj, new TypeReference<>() {});
+        MultiValueMap<String, String> valueMap = new LinkedMultiValueMap<>();
+        valueMap.setAll(fieldMap);
+
+        return UriComponentsBuilder.newInstance()
+                .queryParams(valueMap)
+                .encode()
+                .build()
+                .getQuery();
+    }
+
+    public String encode2(Object obj) {
+        Map<String, String> fieldMap = mapper.convertValue(obj, new TypeReference<>() {});
+        MultiValueMap<String, String> valueMap = new LinkedMultiValueMap<>();
+        valueMap.setAll(fieldMap);
+
+        return UriComponentsBuilder.newInstance()
+                .queryParams(valueMap)
+                .encode()
+                .build()
+                .getPath();
+    }
+
+}
