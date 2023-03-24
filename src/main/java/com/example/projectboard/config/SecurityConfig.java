adfc620352ab4caeafceaed45c3fb085cc2a6416
Jyuka <yoyksks3@gmail.com>
2023-03-24 23:25:46 +0900

    SecurityConfig 설정 : 인증 필요 url 변경, static resource 권한 설정, UserDetailsService, PasswordEncoder 구현
    (1) 기존 SpringSecurityConfig 설정 anyRequest().permitAll() 에서 특정 url 에서만 인증없이 열람이 허용되도록 설정 변경
    
    (2) static resource 에 대한 허용
        - WebSecurityCustomizer ignore 를 설정하는 순간 스프링 시큐리티의 기본 설정 등 컨텍스트의 관리에서 벗어나게 된다.
        - 이로 인해 css, javascript 에서 동작하는 공격 및 csrf 에 취약하여 권장되지 않는다.
        - SecurityFilterChain 의 permitAll 를 사용하는 것을 권장한다.
        - You are asking Spring Security to ignore org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest$StaticResourceRequestMatcher@74b7fb89.
        - This is not recommended -- please use permitAll via HttpSecurity#authorizeHttpRequests instead.
    
    (3) UserDetailsService 구현
        - UserDetailsService Functional Interface 람다식으로 구현
    
    (4) PasswordEncoder
        - Spring 에서 제공하는 PasswordEncoderFactories 를 이용하여 패스워드 암호화 구현

--- src/main/java/com/example/projectboard/config/SecurityConfig.java
+++ src/main/java/com/example/projectboard/config/SecurityConfig.java
@@ -1,19 +1,72 @@
 package com.example.projectboard.config;
 
+import com.example.projectboard.dto.UserAccountDto;
+import com.example.projectboard.dto.security.BoardPrincipal;
+import com.example.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig{
-
+    // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin().and()
+                .logout().logoutSuccessUrl("/").and()
                 .build();
+    }
+
+    /**
+     * ignore 를 설정하는 순간 스프링 시큐리티의 기본 설정 등 컨텍스트의 관리에서 벗어나게 된다.
+     * 이로 인해 css, javascript 에서 동작하는 공격 및 csrf 에 취약하여 권장되지 않는다.
+     * SecurityFilterChain 의 permitAll 를 사용하는 것을 권장한다.
+     * You are asking Spring Security to ignore org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest$StaticResourceRequestMatcher@74b7fb89.
+     * This is not recommended -- please use permitAll via HttpSecurity#authorizeHttpRequests instead.
+     */
+//    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // static resource --> css, js
+//        return (web) -> web.ignoring().antMatchers("/css");
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+
+    /**
+     * UserDetailsService Functional Interface 람다식으로 구현
+     * @return BoardPrincipal implements UserDetails
+     */
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository){
+        return username -> userAccountRepository.findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username : " + username))
+                ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
