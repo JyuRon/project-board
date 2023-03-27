51e4a9558b884132c4eff74d3787aa506ae779e4
Jyuka <yoyksks3@gmail.com>
2023-03-27 16:04:16 +0900

    [도메인 변경점의 반영: 해시태그] Article Entity ManyToMay 연관관계 설정
    (1) @JoinTable
    - @ManyToMany 사용 시 꼭 필요한 설정은 아니지만 테이블명, 컬럼명을 직접 지정을 해주기 위한 설정
    - 실제 소유주 Entity 에서 설정한다.
    - @JoinTable(joinColumns) : 주인 Entity의 column 을 입력한다.
    - @JoinTable(inverseJoinColumns) : 연관관계 Entity의 column 을 입력한다.
    
    (2) @ManyToMany (hashtag column 삭제 이후 Hashtag 연관 관계 설정)
    - LinkedHashSet : 순서(Sort)를 유지하기 위함
    - CascadeType.PERSIST : insert 작업 발생 시 수행
    - CascadeType.MERGE : update 작업 발생 시 수행
    
    (3) 해시태그 엔티티 추가 및 초기화 wrapping

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -13,7 +14,6 @@ import java.util.Set;
 @ToString(callSuper = true) // 상속한 부모 클래스의 정보도 포함한다.
 @Table(indexes = {
         @Index(columnList = "title"),
-        @Index(columnList = "hashtag"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
@@ -35,9 +35,27 @@ public class Article extends AuditingFields{
     @Setter
     @Column(nullable = false, length = 10000)
     private String content; // 본문
-    @Setter
-    private String hashtag; // 해시태그
 
+    /**
+     * @ManyToMany 사용 시 꼭 필요한 설정은 아니지만 직접 지정을 해주기 위한 설정
+     * 실제 소유주 Entity 에서 설정한다.
+     *
+     * @JoinTable(joinColumns) : 주인 Entity의 column 을 입력한다.
+     * @JoinTable(inverseJoinColumns) : 연관관계 Entity의 column 을 입력한다.
+     *
+     * LinkedHashSet : 순서(Sort)를 유지하기 위함
+     *
+     * CascadeType.PERSIST : insert 작업 발생 시 수행
+     * CascadeType.MERGE : update 작업 발생 시 수행
+     */
+    @JoinTable(
+            name= "article_hashtag",
+            joinColumns = @JoinColumn(name = "articleId"),
+            inverseJoinColumns = @JoinColumn(name = "hashtagId")
+    )
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}) // insert, update 시
+    private Set<Hashtag> hashtags = new LinkedHashSet<>(); // LinkedHashSet : 순서를 유지하기 위함
 
     @ToString.Exclude
     @OrderBy("createdAt DESC")
@@ -48,15 +66,26 @@ public class Article extends AuditingFields{
     protected Article() {
     }
 
-    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content) {
         this.userAccount = userAccount;
         this.title = title;
         this.content = content;
-        this.hashtag = hashtag;
     }
 
-    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
-        return new Article(userAccount, title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content) {
+        return new Article(userAccount, title, content);
+    }
+
+    public void addHashtag(Hashtag hashtag) {
+        this.getHashtags().add(hashtag);
+    }
+
+    public void addHashtags(Collection<Hashtag> hashtags) {
+        this.getHashtags().addAll(hashtags);
+    }
+
+    public void clearHashtags() {
+        this.getHashtags().clear();
     }
 
     // id(영속화)가 존재 하지 않다면 모두 다른 객체로 취급
