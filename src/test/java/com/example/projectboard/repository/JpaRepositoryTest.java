88cf5785d420c2e78f81d5f6a47cab1e6595fdd9
Jyuka <yoyksks3@gmail.com>
2023-03-28 22:18:42 +0900

    대댓글 테스트 데이터 업데이트 + 대댓글 쿼리 동작을 확인하기 위한 JPA 테스트 추가
    - 새로 도메인이 바뀌었으므로 테스트 데이터 밀어넣는 쿼리문도 대응을 해주어야 한다.
    - 또한 jpa 테스트를 통해 사용할 쿼리 기능들을 미리 테스트함

--- src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.ArticleComment;
 import com.example.projectboard.domain.Hashtag;
 import com.example.projectboard.domain.UserAccount;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -95,7 +98,7 @@ class JpaRepositoryTest {
         assertThat(savedArticle.getHashtags())
                 .hasSize(1)
                 .extracting("hashtagName", String.class)
-                .containsExactly(updatedHashtag.getHashtagName())
+                    .containsExactly(updatedHashtag.getHashtagName())
         ;
 
     }
@@ -131,6 +134,73 @@ class JpaRepositoryTest {
                 ;
     }
 
+    @DisplayName("대댓글 조회 테스트")
+    @Test
+    void givenParentCommentId_whenSelecting_thenReturnsChildComments(){
+        //Given
+
+        //When
+        Optional<ArticleComment> parentComment = articleCommentRepository.findById(1L);
+
+        //then
+        // assertThat.get() : Optional 에 대한 테스트 코드
+        assertThat(parentComment).get()
+                .hasFieldOrPropertyWithValue("parentCommentId", null) // 필드 검사
+                .extracting("childComments", InstanceOfAssertFactories.COLLECTION)
+                .hasSize(4)
+        ;
+    }
+
+    @DisplayName("댓글에 대댓글 삽입 테스트")
+    @Test
+    void givenParentComment_whenSaving_thenInsertsChildComment() {
+        // Given
+        ArticleComment parentComment = articleCommentRepository.getReferenceById(1L);
+        ArticleComment childComment = ArticleComment.of(
+                parentComment.getArticle(),
+                parentComment.getUserAccount(),
+                "대댓글"
+        );
+
+        // When
+        parentComment.addChildComment(childComment);
+        articleCommentRepository.flush();
+
+        // Then
+        assertThat(articleCommentRepository.findById(1L)).get()
+                .hasFieldOrPropertyWithValue("parentCommentId", null)
+                .extracting("childComments", InstanceOfAssertFactories.COLLECTION)
+                    .hasSize(5)
+        ;
+    }
+
+    @DisplayName("댓글 삭제와 대댓글 전체 연동 삭제 테스트")
+    @Test
+    void givenArticleCommentHavingChildComments_whenDeletingParentComment_thenDeletesEveryComment() {
+        // Given
+        ArticleComment parentComment = articleCommentRepository.getReferenceById(1L);
+        long previousArticleCommentCount = articleCommentRepository.count();
+
+        // When
+        articleCommentRepository.delete(parentComment);
+
+        // Then
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - 5); // 테스트 댓글 + 대댓글 4개
+    }
+
+    @DisplayName("댓글 삭제와 대댓글 전체 연동 삭제 테스트 - 댓글 ID + 유저 ID")
+    @Test
+    void givenArticleCommentIdHavingChildCommentsAndUserId_whenDeletingParentComment_thenDeletesEveryComment() {
+        // Given
+        long previousArticleCommentCount = articleCommentRepository.count();
+
+        // When
+        articleCommentRepository.deleteByIdAndUserAccount_UserId(1L, "jyuka");
+
+        // Then
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - 5); // 테스트 댓글 + 대댓글 4개
+    }
+
     @DisplayName("[Querydsl] 전체 hashtag 리스트에서 이름만 조회하기")
     @Test
     void givenNothing_whenQueryingHashtags_thenReturnsHashtagNames() {
@@ -161,12 +231,10 @@ class JpaRepositoryTest {
         assertThat(articlePage.getContent().get(0).getTitle()).isEqualTo("Fusce posuere felis sed lacus.");
         assertThat(articlePage.getContent().get(0).getHashtags())
                 .extracting("hashtagName", String.class)
-                .containsExactly("fuscia");
+                    .containsExactly("fuscia");
         assertThat(articlePage.getTotalElements()).isEqualTo(17);
         assertThat(articlePage.getTotalPages()).isEqualTo(4);
     }
-
-
     @EnableJpaAuditing
     @TestConfiguration
     static class TestJpaConfig{
